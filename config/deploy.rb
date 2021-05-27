4e2e1896e3d5e24219f9eb55418a8200d83e7ef4
Michelium <michelhamelink@gmail.com>
2021-05-27 17:42:59 +0200

    updated package-lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ set :tmp_dir, "/home/u98337p92543/tmp"
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
